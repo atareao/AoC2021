88815fe70ef4602ffa62d9fb713fdfedc8c9350a
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2021-12-09 19:21:03 +0100

    improve part 1 for part 2

--- puz09/src/functions.rs
+++ puz09/src/functions.rs
@@ -1,9 +1,21 @@
-use std::convert::TryInto;
-
 use log::debug;
 
 
 pub fn part1(data: &str) -> u32{
+    let mut field = get_field(data);
+    let low_points = get_low_points(&field);
+    let mut risk_level = 0;
+    for low_point in low_points{
+        risk_level += field[low_point.0][low_point.1] + 1;
+    }
+    risk_level
+}
+
+pub fn part2(data: &str) -> i32 {
+    1
+}
+
+fn get_field(data: &str) -> Vec<Vec<u32>> {
     let width = data.lines().next().unwrap().chars().count();
     let height = data.lines().count();
     debug!("widht:{}, height: {}", width, height);
@@ -15,21 +27,23 @@ pub fn part1(data: &str) -> u32{
             }
         }
     }
-    let mut risk_level = 0;
+    return dots
+}
+
+fn get_low_points(field: &Vec<Vec<u32>>) -> Vec<(usize, usize)>{
+    let width = field.len();
+    let height = field[0].len();
+    let mut low_points: Vec<(usize, usize)> = Vec::new();
     for x in 0..width{
         for y in 0..height{
-            if (x == 0 || dots[x][y] < dots[x-1][y]) &&
-                (x == width - 1 || dots[x][y] < dots[x+1][y]) &&
-                (y == 0 || dots[x][y] < dots[x][y-1]) &&
-                (y == height - 1 || dots[x][y] < dots[x][y+1]) {
-                    debug!("({},{}) => {}", x, y, dots[x][y]);
-                    risk_level += dots[x][y] + 1;
+            if (x == 0 || field[x][y] < field[x-1][y]) &&
+                (x == width - 1 || field[x][y] < field[x+1][y]) &&
+                (y == 0 || field[x][y] < field[x][y-1]) &&
+                (y == height - 1 || field[x][y] < field[x][y+1]) {
+                    debug!("({},{}) => {}", x, y, field[x][y]);
+                    low_points.push((x, y));
             }
         }
     }
-    risk_level
-}
-
-pub fn part2(data: &str) -> i32 {
-    1
+    low_points
 }
