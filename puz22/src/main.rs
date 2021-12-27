8d0bde3e56411fb12ac13c26cb12df2ba080b3ff
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2021-12-27 10:11:13 +0100

    Complete puz22

--- puz22/src/main.rs
+++ puz22/src/main.rs
@@ -15,6 +15,6 @@ fn main() {
     let data = &read_data_from_url("https://adventofcode.com/2021/day/22/input");
     let resultado_part1 = part1(data);
     println!("Resultado parte 1: {}", resultado_part1);
-    //let resultado_part2 = part2(data);
-    //println!("Resultado parte 2: {}", resultado_part2);
+    let resultado_part2 = part2(data);
+    println!("Resultado parte 2: {}", resultado_part2);
 }
